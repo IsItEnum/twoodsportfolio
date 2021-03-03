2b73c673a031690bfcc2a367675ad66a604df637
ZOINKS <tm-woods@hotmail.com>
2021-03-02 23:13:52 -0500

    Updates

--- precache-manifest.ee98447de14b23b41ce3c17d0da6df9c.js
+++ precache-manifest.ee98447de14b23b41ce3c17d0da6df9c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "658d7e6cb17096bba849",
+    "revision": "184cb99fb7854cff3b81",
     "url": "/twoodsportfolio/static/css/main.9bd85206.chunk.css"
   },
   {
-    "revision": "658d7e6cb17096bba849",
-    "url": "/twoodsportfolio/static/js/main.658d7e6c.chunk.js"
+    "revision": "184cb99fb7854cff3b81",
+    "url": "/twoodsportfolio/static/js/main.184cb99f.chunk.js"
   },
   {
     "revision": "079682ea17c1f3894440",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/twoodsportfolio/static/js/2.5ec21d73.chunk.js"
   },
   {
-    "revision": "4bf1d152c8378fd3cd022ad40d69c582",
+    "revision": "5722639f1922462354a646e5b1e08bd1",
     "url": "/twoodsportfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/twoodsportfolio/precache-manifest.6e8afbf4fcd9f2fdea2fb5b230662c79.js"
+  "/twoodsportfolio/precache-manifest.ee98447de14b23b41ce3c17d0da6df9c.js"
 );
 
 workbox.clientsClaim();
